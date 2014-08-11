37508ba0652e5ed0f5cbb88bf122ca9d8fb87f80
Joel Hans <joel.g.hans@gmail.com>
2014-08-11 11:07:29 -0700

    Rolled out two more pieces.

--- assets/javascripts/build/chapbooks/prepare-to-die/prepare-to-die.js
+++ assets/javascripts/build/chapbooks/prepare-to-die/prepare-to-die.js
@@ -1,8 +1,57 @@
 (function() {
   jQuery(function($) {
-    return $(window).stellar({
+    $(window).stellar({
       horizontalScrolling: false,
       hideDistantElements: false
+    });
+    return $('#fleets-of-labor-made-this-marked-infrastructure').waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(1)').toggleClass('fleets-show');
+    }, {
+      offset: 100
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(2)').toggleClass('fleets-show');
+    }, {
+      offset: 50
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(3)').toggleClass('fleets-show');
+    }, {
+      offset: 0
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(4)').toggleClass('fleets-show');
+    }, {
+      offset: -50
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(5)').toggleClass('fleets-show');
+    }, {
+      offset: -100
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(6)').toggleClass('fleets-show');
+    }, {
+      offset: -150
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(1)').toggleClass('fleets-show');
+    }, {
+      offset: -250
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(2)').toggleClass('fleets-show');
+    }, {
+      offset: -300
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(3)').toggleClass('fleets-show');
+    }, {
+      offset: -350
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(4)').toggleClass('fleets-show');
+    }, {
+      offset: -400
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(5)').toggleClass('fleets-show');
+    }, {
+      offset: -450
+    }).waypoint(function() {
+      return $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(6)').toggleClass('fleets-show');
+    }, {
+      offset: -500
     });
   });
 
--- assets/javascripts/source/chapbooks/prepare-to-die/prepare-to-die.coffee
+++ assets/javascripts/source/chapbooks/prepare-to-die/prepare-to-die.coffee
@@ -1,5 +1,43 @@
 jQuery ($) ->
-  
+
   $(window).stellar
     horizontalScrolling: false
     hideDistantElements: false
+
+  $('#fleets-of-labor-made-this-marked-infrastructure')
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(1)').toggleClass('fleets-show')
+  , offset: 100
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(2)').toggleClass('fleets-show')
+  , offset: 50
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(3)').toggleClass('fleets-show')
+  , offset: 0
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(4)').toggleClass('fleets-show')
+  , offset: -50
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(5)').toggleClass('fleets-show')
+  , offset: -100
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(6)').toggleClass('fleets-show')
+  , offset: -150
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(1)').toggleClass('fleets-show')
+  , offset: -250
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(2)').toggleClass('fleets-show')
+  , offset: -300
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(3)').toggleClass('fleets-show')
+  , offset: -350
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(4)').toggleClass('fleets-show')
+  , offset: -400
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(5)').toggleClass('fleets-show')
+  , offset: -450
+  .waypoint () ->
+    $('.fleets-of-labor-made-this-marked-infrastructure p:nth-of-type(6)').toggleClass('fleets-show')
+  , offset: -500
